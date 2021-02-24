a4c4669d93117976ca6b291321c4743ae7a4991a
seanstephens17 <D00211442@student.dkit.ie>
2021-02-24 23:01:30 +0000

    AppTest Updated

--- src/test/java/dkit/oop/AppTest.java
+++ src/test/java/dkit/oop/AppTest.java
@@ -22,7 +22,7 @@ public class AppTest
     {
         Student s1 = new Student(1234567,"20-04-12","asdfhe", "sean@shejd.com");
 
-        Student s2 = new Student(s1);
+        Student s2 = new Student(s1); //clone
 
         assert (s1 != s2);
 
@@ -31,5 +31,6 @@ public class AppTest
         s2.setCaoNumber(6475838);
 
         assert(!s1.equals(s2));
+
     }
 }
